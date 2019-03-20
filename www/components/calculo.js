59bac5666e855570687ed0372c46d7ccdc9d713a
Carlos <carlos.coutinho5@etec.sp.gov.br>
2019-03-21 01:09:17 +0900

    Finalização do calculo, estilo de resposta final, adaptação do titulo e remoção do h1.

--- www/components/calculo.js
+++ www/components/calculo.js
@@ -9,17 +9,19 @@ $(document).on("click","#btnResultado",function(){
     altura = $("#altura").val();
     peso = $("#kg").val(); 
     idade = $("#idade").val();
-    sexo = $("#sexo").val(this);
-    atvFisica = $("#atvFisica").val(this);
+    sexo = $("#btnSexo").val();
+    atvFisica = $("#atvFisica").val();
 
-    if(sexo == "masc"){
+    if(sexo == "masc"){  
       resultado = atvFisica * (66 + ((13.7* peso) + (5* altura) - (6.8 * idade)));
     }
     else{
       resultado = atvFisica * (655 + ((9.6* peso) + (1.8* altura) - (4.7 * idade)));
     }
-
-    $("#resposta").val("Você gasta " + resultado +" kcal p/Dia");
     
-    alert("funciona");
+    $("#resposta").val(resultado.toFixed(0));
+    $("#detalhes").val("kcal/Dia");
+    $("#titulo").val("VOCÊ");
+    $("#titulo2").val("GASTA:");
+    
 });
